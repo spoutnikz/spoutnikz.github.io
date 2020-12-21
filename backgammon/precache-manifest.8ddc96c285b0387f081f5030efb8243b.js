67bbb2e871925235372795e9b42b56361eda06f3
spoutnik <bce@rtbf.be>
2020-12-21 20:33:00 +0100

    backgammon

---
+++ backgammon/precache-manifest.8ddc96c285b0387f081f5030efb8243b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d1fb5f42fa152a3f909",
+    "url": "/css/app.61a811e0.css"
+  },
+  {
+    "revision": "31606e9a7d08a435102254383e90f434",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d1fb5f42fa152a3f909",
+    "url": "/js/app.e6a6692f.js"
+  },
+  {
+    "revision": "06387b25f6b86ad44633",
+    "url": "/js/chunk-vendors.7075acc7.js"
+  },
+  {
+    "revision": "cdc2743b240e4c6dc5ab70a4794ca002",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
